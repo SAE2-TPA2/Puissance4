c1898300460a2eb5eb4761e44da4c3d54b8ff9d5
François SP <francois-sp@gmx.fr>
2024-01-31 11:54:20 +0100

    Ajout attribut pour savoir si l joueur est humain ou IA

--- App/Engine/Joueur.py
+++ App/Engine/Joueur.py
@@ -1,9 +1,11 @@
 from Jeton import Rond, Croix
 
+
 class Joueur:
-    def __init__(self, pseudo: str, jeton: Rond | Croix):
+    def __init__(self, pseudo: str, jeton: Rond | Croix, est_humain: bool = True):
         self.pseudo = pseudo
         self.jeton = jeton
+        self.est_humain = est_humain
 
     def __str__(self):
         return f"{self.pseudo} avec le jeton {self.jeton}"
