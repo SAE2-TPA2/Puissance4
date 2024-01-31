ee721834c7caffb6dbee70589487811546c18ee1
François SP <francois-sp@gmx.fr>
2024-01-31 11:20:14 +0100

    Création de la classe Jton

---
+++ App/Engine/Jeton.py
@@ -0,0 +1,14 @@
+class Rond:
+    def __init__(self):
+        self.caractere = "O"
+
+    def __str__(self):
+        return self.caractere
+
+
+class Croix:
+    def __init__(self):
+        self.caractere = "X"
+
+    def __str__(self):
+        return self.caractere
