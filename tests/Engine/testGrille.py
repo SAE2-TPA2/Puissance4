5b2d531beb20d1b804863a3343400e6838ddec5b
quentinformatique <quentin161204@gmail.com>
2024-01-31 11:58:06 +0100

    ajout tests get colonne

--- tests/Engine/testGrille.py
+++ tests/Engine/testGrille.py
@@ -1,14 +1,20 @@
 import unittest
-from Engine.Grille import Grille
+from App.Engine.Grille import Grille
+
 
 class TestGrille(unittest.TestCase):
 
-    def test_getColonne(self):
+    def test_get_grille(self):
         grille = Grille()
-        grille.getColonne(0)
-        grille.getColonne(6)
-        with self.assertRaises(Exception):
-            grille.getColonne(-1)
-        with self.assertRaises(Exception):
+        self.assertEqual(grille.getGrille(), [[None for x in range(7)] for y in range(6)])
+
+    def test_get_colonne(self):
+        grille = Grille()
+        self.assertEqual(grille.getColonne(4), [None for x in range(6)])
+        with self.assertRaises(IndexError):
             grille.getColonne(7)
+        with self.assertRaises(IndexError):
+            grille.getColonne(-1)
+
+
 
