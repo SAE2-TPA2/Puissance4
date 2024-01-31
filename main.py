763f63c975a029747658d4d43a19f8e896733cb8
François SP <francois-sp@gmx.fr>
2024-01-31 11:54:26 +0100

    Debut du menu

---
+++ main.py
@@ -0,0 +1,20 @@
+#from App.Partie import Partie
+
+if __name__ == '__main__':
+
+    # Affichage menu principal et choix de l'utilisateur
+    # 1. Joueur vs Joueur
+    # 2. Joueur vs IA
+    # 3. IA vs IA
+    # 4. Quitter
+
+    print("Bienvenue dans le jeu du Puissance 4 !")
+    print("Sélectionnez le mode de jeu :")
+    print("1. Joueur vs Joueur")
+    print("2. Joueur vs IA")
+    print("3. IA vs IA")
+    print("4. Quitter")
+
+    choix = input("Votre choix : ")
+
+    
