b10bb2c94d293fbe958b62c3b675d6120cc04b34
Alexandreiut <alexandre.brouzes@iut-rodez.fr>
2024-02-01 14:48:12 +0100

    Complétion de la méthode boucle jeu

--- App/Partie.py
+++ App/Partie.py
@@ -5,23 +5,58 @@ from Engine.Grille import Grille
 
 class Partie:
     def __init__(self):
-        self.nbTour = 0
-        self.joueur1 = Joueur("",Croix())
-        self.joueur2 = Joueur("",Rond())
+        self.nb_tour = 1
+        self.joueur_1 = Joueur("",Croix())
+        self.joueur_2 = Joueur("",Rond())
         self.grille = Grille()
 
-    def placerPion(self,colonne : int):
-        """ Place un pion en fonction d'un numéro de colonne
-        type de pion déterminer avec le numéro du tour
+    def get_nb_tour(self):
+        return self.nb_tour
+    
+    def get_joueur_1(self):
+        return self.joueur_1
+    
+    def get_joueur_2(self):
+        return self.joueur_2
+    
+    def get_grille(self):
+        return self.grille
 
-
-        Args:
-            colonne (int): entier compris entre 1 et 7
+    def boucle_jeu(self):
+        """Le jeu se déroule tant que la grille n'est pas pleine ou 
+        que le jeu est considéré comme terminé(gagné,perdu,égalité)
+        les joueurs vont jouer à tour de rôle jusqu'a la fin du jeu 
         """
-        pass
-
-    def boucleJeu(self):
-        """
-
-        """
-        pass
+        nb_tour_joue = 1
+        
+        while self.grille.est_gagner() is None\
+            or self.grille.grille_est_pleine():
+            jeton_joue = ""    
+            if(nb_tour_joue % 2 != 0):
+                nb_tour_joue += 1
+                while True:
+                    #try:
+                        numero_colonne_joue = jouer_pion(self.joueur_1)
+                        break
+                    #except nom_exception: TODO compléter avec le nom de l'exception correspondante
+                        # ...    
+                jeton_joue = "Croix"               
+            else:
+                nb_tour_joue += 1
+                while True:
+                    #try:
+                        numero_colonne_joue = jouer_pion(self.joueur_2)
+                        break
+                    #except nom_exception: TODO compléter avec le nom de l'exception correspondante
+                        # ...    
+                jeton_joue = "Rond"
+            
+            self.grille.placer_pion(numero_colonne_joue,jeton_joue)
+        if(self.est_gagner == "Croix"):
+            print("Le joueur ",self.joueur_1.get_pseudo(), "remporte la partie !")
+        else:
+            if(self.est_gagner == "Rond"):
+                print("Le joueur ",self.joueur_2.get_pseudo(), "remporte la partie !")
+            else: 
+                print("Égalité, aucun joueur ne remporte la partie")
+        
