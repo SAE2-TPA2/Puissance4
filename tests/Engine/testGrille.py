70efdc5c330d6730dba0017461cc4987882569fb
quentinformatique <quentin161204@gmail.com>
2024-02-01 14:36:38 +0100

    rename method

--- tests/Engine/testGrille.py
+++ tests/Engine/testGrille.py
@@ -67,8 +67,7 @@ class TestGrille(unittest.TestCase):
         with self.assertRaises(IndexError):
             grille.placer_pion(-1, rond)
 
-
-    def test_est_pleine(self):
+    def test_grille_est_pleine(self):
         grille = Grille()
         self.assertFalse(grille.est_pleine())
         for i in range(6):
