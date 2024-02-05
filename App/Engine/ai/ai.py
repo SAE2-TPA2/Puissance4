5508178f88bc45072f9a42c9e618badb68cb8312
François SP <francois-sp@gmx.fr>
2024-02-05 08:50:21 +0100

    Entete pour IA

---
+++ App/Engine/ai/ai.py
@@ -0,0 +1,22 @@
+from App.Engine import Grille
+from App.Engine.Jeton import Jeton
+
+
+def min_max(grille: Grille) -> int:
+    """
+    Méthode min-max pour choisir la colonne à jouer
+    :param grille: la grille du jeu
+    :return: L'indice de la colonne à jouer
+    """
+    pass
+
+
+def evaluation(grille: Grille, mon_pion: Jeton, pion_qui_joue: Jeton) -> int:
+    """
+    Fonction d'évaluation pour la méthode min-max
+    :param pion_qui_joue: Le pion qui joue
+    :param mon_pion: Mon pion
+    :param grille: la grille du jeu
+    :return: la valeur de la grille
+    """
+    pass
