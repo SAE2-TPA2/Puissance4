734133af1e3c63bf4e81d65ff22780e10c1b291c
François SP <francois-sp@gmx.fr>
2024-02-06 17:09:33 +0100

    Clean code

--- app/engine/ai/ai.py
+++ app/engine/ai/ai.py
@@ -2,14 +2,11 @@ from app.engine.grille import Grille
 from app.engine.jeton import Jeton
 
 
-def est_terminal(grille) -> bool:
-    pass
-
-
-def min(fils: list[tuple[int, int]]) -> tuple[int, int]:
+def min_tuple(fils: list[tuple[int, int]]) -> tuple[int, int]:
     """
     Retourne le minimum d'une liste de tuple
     Les tuples sont de la forme (coups, valeur)
+    On recherche le minimum de la valeur
     :param fils:
     :return:
     """
@@ -22,10 +19,11 @@ def min(fils: list[tuple[int, int]]) -> tuple[int, int]:
     return min_coup, min_valeur
 
 
-def max(fils: list[tuple[int, int]]) -> tuple[int, int]:
+def max_tuple(fils: list[tuple[int, int]]) -> tuple[int, int]:
     """
     Retourne le maximum d'une liste de tuple
     Les tuples sont de la forme (coups, valeur)
+    On recherche le maximum de la valeur
     :param fils:
     :return:
     """
@@ -46,18 +44,7 @@ def min_max(grille: Grille, mon_pion: Jeton, pion_qui_joue: Jeton) -> int:
     :param grille: la grille du jeu
     :return: L'indice de la colonne à jouer
     """
-    if est_terminal(grille):
-        return evaluation(grille, mon_pion, pion_qui_joue)
-    else:
-        coups_possible = grille.coups_possible()
-        coups_possible_valeur = []
-        for etat in coups_possible:
-            coups_possible_valeur.append(min_max(etat, mon_pion, pion_qui_joue))
-
-        if pion_qui_joue == mon_pion:
-            return max(coups_possible_valeur)
-        else:
-            return min(coups_possible_valeur)
+    pass
 
 
 def evaluation(grille: Grille, mon_pion: Jeton, pion_qui_joue: Jeton) -> int:
@@ -69,18 +56,3 @@ def evaluation(grille: Grille, mon_pion: Jeton, pion_qui_joue: Jeton) -> int:
     :return: la valeur de la grille
     """
     pass
-
-
-def etat_suivant(grille: Grille, pion: Jeton) -> list[Grille]:
-    """
-    Retourne l'état de la grille après avoir joué un coup
-    :param grille: la grille du jeu
-    :param pion: le pion à jouer
-    :return: la grille après avoir joué le coup
-    """
-    resultat: list[Grille] = []
-    for coup in grille.coups_possible():
-        grille_copie = grille.__deepcopy__()
-        grille_copie.jouer_pion(coup, pion)
-        resultat.append(grille_copie)
-    return resultat
